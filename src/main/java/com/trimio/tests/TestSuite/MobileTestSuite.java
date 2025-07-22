9062bc20b461f003f3ddcba97850bafa71ba56ba
pharemedia1 <45445324+pharemedia1@users.noreply.github.com>
2025-07-22 17:08:34 -0400

    Updated test suite skeleton

--- src/main/java/com/trimio/tests/TestSuite/MobileTestSuite.java
+++ src/main/java/com/trimio/tests/TestSuite/MobileTestSuite.java
@@ -1,4 +1,14 @@
 package com.trimio.tests.TestSuite;
+import com.trimio.tests.*;
+import com.trimio.tests.AppointmentFlow.TrimioScheduleAppointmentTest;
+import com.trimio.tests.Base.AppiumBase;
+import com.trimio.tests.Login.TrimioLoginTest;
 
 public class MobileTestSuite {
+    TrimioLoginTest loginTest = new TrimioLoginTest();
+    TrimioScheduleAppointmentTest appointmentTest = new TrimioScheduleAppointmentTest();
+
+    public static void main(String[] args) {
+
+    }
 }
